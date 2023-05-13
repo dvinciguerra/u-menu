2157a232ec074cc6c48d8e8ae18aba289d504170
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2023-05-13 15:51:47 -0300

    feat: add public interface for icons and configurations

--- lib/micro/menu.rb
+++ lib/micro/menu.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
+require_relative 'menu/configurations'
+require_relative 'menu/icons'
 require_relative 'menu/version'
+
+require 'pastel'
 
 module Micro
   module Menu
     class Error < StandardError; end
+
+    def self.configurations
+      Configurations
+    end
+
+    def self.icons
+      Icons
+    end
+
+    def self.colors
+      @colors ||= Pastel.new
+    end
   end
 end
--- spec/micro/menu_spec.rb
+++ spec/micro/menu_spec.rb
@@ -5,7 +5,21 @@ RSpec.describe Micro::Menu do
     expect(Micro::Menu::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.icons' do
+    it 'returns icons module' do
+      expect(described_class.icons).to be(Micro::Menu::Icons)
+    end
+  end
+
+  describe '.configurations' do
+    it 'returns configurations module' do
+      expect(described_class.configurations).to be(Micro::Menu::Configurations)
+    end
+  end
+
+  describe '.colors' do
+    it 'returns instance of pastel' do
+      expect(described_class.colors).to eq(Pastel.new)
+    end
   end
 end
