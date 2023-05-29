2cc1c5de46536362ea30df2f4d5b21293578c8ff
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2023-05-29 18:45:15 -0300

    test: add securerandom to tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@
 
 require 'u-menu'
 
+require 'securerandom'
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
